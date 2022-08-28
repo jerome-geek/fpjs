81a1953784f2069abb4133414e53be61a82d3166
jerome <jerome@geekstudio.co.kr>
2022-08-28 14:05:31 +0900

    :memo: add: 4-4. go + curry를 사용하여 더 읽기 좋은 코드로 만들기

--- 함수형 프로그래밍과 JavaScript ES6+/lib/fx.js
+++ 함수형 프로그래밍과 JavaScript ES6+/lib/fx.js
@@ -1,14 +1,20 @@
 const log = console.log;
 
-const map = (f, iter) => {
+const curry =
+    (f) =>
+    (a, ..._) =>
+        _.length ? f(a, ..._) : (..._) => f(a, ..._);
+
+// 아래 함수들을 curry로 감싸주어 인자를 하나만 받을 경우 이후 인자들을 더 받기로 기다리는 함수를 리턴하는 형태로 바뀜
+const map = curry((f, iter) => {
     let res = [];
     for (const p of iter) {
         res.push(f(p));
     }
     return res;
-};
+});
 
-const filter = (f, iter) => {
+const filter = curry((f, iter) => {
     let res = [];
     for (const a of iter) {
         if (f(a)) {
@@ -16,9 +22,9 @@ const filter = (f, iter) => {
         }
     }
     return res;
-};
+});
 
-const reduce = (f, acc, iter) => {
+const reduce = curry((f, acc, iter) => {
     // iter가 없는 경우 acc가 iter
     if (!iter) {
         iter = acc[Symbol.iterator]();
@@ -29,4 +35,4 @@ const reduce = (f, acc, iter) => {
         acc = f(acc, a);
     }
     return acc;
-};
+});
