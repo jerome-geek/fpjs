83d110509f349b3525976d4d4cd404c890511661
jerome <jerome@geekstudio.co.kr>
2024-07-07 15:22:30 +0900

    타입 잘 다루기 3/5 - Array 유틸 함수

--- share/main.ts
+++ share/main.ts
@@ -35,3 +35,52 @@ check<Head<[1, 2, 3, 4]>, 1>(Pass);
 check<Head<[1]>, 1>(Pass);
 check<Head<[]>, undefined>(Pass);
 check<Head<[]>, 2>(Fail);
+
+// never = 공집합
+type Length<T extends any[]> = T['length'];
+
+check<Length<[1, 2, 3]>, 3>(Pass);
+
+type HasTail<T extends any[]> = Length<T> extends 0 ? false : true;
+check<HasTail<[1, 2, 3]>, true>(Pass);
+check<HasTail<[3]>, true>(Pass);
+check<HasTail<[]>, false>(Pass);
+
+type Tail<T extends any[]> = T extends [any, ...infer A] ? A : [];
+check<Tail<[1, 2, 3, 4, 5]>, [2, 3, 4, 5]>(Pass);
+check<Tail<[4, 5]>, [5]>(Pass);
+check<Tail<[4]>, []>(Pass);
+check<Tail<[]>, []>(Pass);
+
+// JS와 다르게 TS에서는 spread operator를 마지막에 쓰지 않아도 된다
+type Last<T extends any[]> = T extends [...any[], infer A] ? A : undefined;
+check<Last<[1, 2, 3, 4]>, 4>(Pass);
+check<Last<[1]>, 1>(Pass);
+check<Last<[]>, undefined>(Pass);
+check<Last<[2]>, undefined>(Fail);
+
+type Prepend<T extends any[], E> = [E, ...T];
+check<Prepend<[], 1>, [1]>(Pass);
+check<Prepend<[3, 4, 5], 2>, [2, 3, 4, 5]>(Pass);
+
+// const a = { '0': 1 as const }['0']; 패턴을 이용해서 구현
+type Drop<N extends number, T extends any[], P extends any[] = []> = {
+    0: T;
+    1: Drop<N, Tail<T>, Prepend<P, any>>;
+}[Length<P> extends N ? 0 : 1];
+check<Drop<3, [1, 2, 3, 4, 5, 6]>, [4, 5, 6]>(Pass);
+check<Drop<0, [1, 2, 3, 4, 5, 6]>, [1, 2, 3, 4, 5, 6]>(Pass);
+check<Drop<7, [1, 2, 3, 4, 5, 6]>, []>(Pass);
+check<Drop<6, [1, 2, 3, 4, 5, 6]>, []>(Pass);
+
+type Reverse<T extends any[], P extends any[] = []> = {
+    0: P;
+    1: Reverse<Tail<T>, Prepend<P, Head<T>>>;
+}[Length<T> extends 0 ? 0 : 1];
+check<Reverse<[1, 2, 3, 4, 5, 6]>, [6, 5, 4, 3, 2, 1]>(Pass);
+
+type Concat<A extends any[], B extends any[]> = [...A, ...B];
+check<Concat<[1, 2, 3], [4, 5, 6]>, [1, 2, 3, 4, 5, 6]>(Pass);
+
+type Append<A extends any[], B> = Concat<A, [B]>;
+check<Append<[1, 2, 3], 4>, [1, 2, 3, 4]>(Pass);
